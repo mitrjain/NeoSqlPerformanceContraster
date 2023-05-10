611802830a9edb9c41bf3cb57dd678eb2605efb9
Mit <mitrjain@gmail.com>
2023-05-10 03:23:39 -0700

    Add .gitignore, Move sql script to root dir

--- initialise_sql.sh
+++ initialise_sql.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 echo "*****Executing command: python createDB.py"
-python createDB.py
+python sql-dbms/createDB.py
 
 echo "*****Executing command python createTables.py"
-python createTables.py
+python sql-dbms/createTables.py
 
 echo "*****Executing command python loadData.py"
-python loadData.py
+python sql-dbms/loadData.py
